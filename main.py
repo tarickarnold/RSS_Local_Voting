c4e23cbd478800c0eaa700f1c9293ea7f6ef7ea6
Tarick Arnold <arnoldt2009@gmail.com>
2024-02-11 18:28:42 -0600

    Added selenium

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 from icecream import ic
 
-r = requests.get('https://www2.tulsacounty.org/electionboard/elections/election-calendar/')
-html = r.text
+url = 'https://www2.tulsacounty.org/electionboard/elections/election-calendar/'
+response = requests.get(url)
+service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(30)
+driver.get(url)
 
-soup = BeautifulSoup(html,'html.parser')
+#Delay
+WebDriverWait(driver,15).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "e-subject")))
 
-ic(soup)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.text)
+
+date = []
+for text in soup.find_all('div',class_='e-time'):
+    date.append(text.get_text())
+    print(date)
