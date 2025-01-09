c26074872b2a6c1b882fd13164f994274b092e3b
Tarick Arnold <120078573+tarickarnold@users.noreply.github.com>
2025-01-09 06:27:23 -0600

    republishing code to repo after error

--- TulsaCounty.py
+++ TulsaCounty.py
@@ -3,16 +3,16 @@ import datetime
 import pandas as pd
 from pandas import json_normalize
 
-try:
-    #Get the first day and last day of the current year
-    starting_day_of_current_year = datetime.datetime.now().replace(month=1, day=1)    
-    ending_day_of_current_year = datetime.datetime.now().replace(month=12, day=31)
+
+#Get the first day and last day of the current year
+starting_day_of_current_year = datetime.datetime.now().replace(month=1, day=1)    
+ending_day_of_current_year = datetime.datetime.now().replace(month=12, day=31)
+
+#Convert first day and last day to string format
+FirstDay = starting_day_of_current_year.strftime('%m/%d/%Y')
+LastDay = ending_day_of_current_year.strftime('%m/%d/%Y')
     
-    #Convert first day and last day to string format
-    FirstDay = starting_day_of_current_year.strftime('%m/%d/%Y')
-    LastDay = ending_day_of_current_year.strftime('%m/%d/%Y')
-    
-    
+try:    
     url = "https://www2.tulsacounty.org/umbraco/BackOffice/Api/AppointmentsApi/LoadElectionData"
     headers = {
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
@@ -37,3 +37,4 @@ except requests.exceptions.RequestException as e:
     # catastrophic error. bail.
     raise SystemExit(e)
 
+print(sorted_df)
